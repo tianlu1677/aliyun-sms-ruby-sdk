84b580a599b6f6ed092574a3f0ecb8ca99bf9237
tianlu1677 <tianlu1677@gmail.com>
2017-03-13 16:22:19 +0800

    format test code

--- test/aliyun/sms/test_phone_code.rb
+++ test/aliyun/sms/test_phone_code.rb
@@ -3,9 +3,9 @@
 require "./test_helper"
 
 class TestPhoneCode < Minitest::Test
-	def setup
-		Aliyun::Sms::PhoneCode.configure do |config|
-			config.access_key_secret = 'testsecret'
+  def setup
+    Aliyun::Sms::PhoneCode.configure do |config|
+      config.access_key_secret = 'testsecret'
       config.access_key_id = 'testid'
       config.action = 'SingleSendSms'
       config.format = 'XML'
@@ -14,11 +14,11 @@ class TestPhoneCode < Minitest::Test
       config.signature_method = 'HMAC-SHA1'
       config.signature_version = '1.0'
       config.sms_version = '2016-09-27'
-		end
-	end
+    end
+  end
 
-	def test_get_sign
-		params_input = {
+  def test_get_sign
+    params_input = {
         'AccessKeyId' => 'testid',
         'Action' => 'SingleSendSms',
         'Format' => 'XML',
@@ -36,10 +36,10 @@ class TestPhoneCode < Minitest::Test
       key = 'testsecret'
       spect_output = 'ka8PDlV7S9sYqxEMRnmlBv%2FDoAE%3D'
       assert_equal spect_output, Aliyun::Sms::PhoneCode.sign(key, params_input)
-	end
+  end
 
-	def test_post_body_data
-		params_input = {
+  def test_post_body_data
+    params_input = {
         'AccessKeyId' => 'testid',
         'Action' => 'SingleSendSms',
         'Format' => 'XML',
@@ -59,6 +59,6 @@ class TestPhoneCode < Minitest::Test
     spect_output = 'Signature=ka8PDlV7S9sYqxEMRnmlBv%2FDoAE%3D&AccessKeyId=testid&Action=SingleSendSms&Format=XML&ParamString={"name":"d","name1":"d"}&RecNum=13098765432&RegionId=cn-hangzhou&SignName=标签测试&SignatureMethod=HMAC-SHA1&SignatureNonce=9e030f6b-03a2-40f0-a6ba-157d44532fd0&SignatureVersion=1.0&TemplateCode=SMS_1650053&Timestamp=2016-10-20T05:37:52Z&Version=2016-09-27'
 
     assert_equal spect_output, Aliyun::Sms::PhoneCode.sign_result(key, params_input)
-	end
+  end
 
 end
