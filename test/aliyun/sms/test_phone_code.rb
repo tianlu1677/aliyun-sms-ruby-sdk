9cc7eddeb5118d0f5cea18997f8d92ae1c0b5cd8
tianlu1677 <tianlu1677@gmail.com>
2017-03-13 11:51:33 +0800

    add minitest

---
+++ test/aliyun/sms/test_phone_code.rb
@@ -0,0 +1,70 @@
+require 'aliyun/sms'
+require 'minitest/autorun'
+
+class TestPhoneCode < Minitest::Test
+	def setup
+		Aliyun::Sms::PhoneCode.configure do |config|
+			config.access_key_secret = 'testsecret'
+      config.access_key_id = 'testid'
+      # config.action = 'SingleSendSms'
+      config.format = 'XML'
+      # config.region_id = 'cn-hangzhou'
+      config.sign_name = '标签测试'
+      # config.signature_method = 'HMAC-SHA1'
+      # config.signature_version = '1.0'
+      # config.sms_version = '2016-09-27'
+		end
+	end
+
+	
+	# def test_get_the_query_string
+	# end
+
+	# def test_get_the_canonicalized_query_string
+	# end
+
+	def test_get_sign
+		params_input = {
+        'AccessKeyId' => 'testid',
+        'Action' => 'SingleSendSms',
+        'Format' => 'XML',
+        'ParamString' => '{"name":"d","name1":"d"}',
+        'RecNum' => '13098765432',
+        'RegionId' => 'cn-hangzhou',
+        'SignName' => '标签测试',
+        'SignatureMethod' => 'HMAC-SHA1',
+        'SignatureNonce' => '9e030f6b-03a2-40f0-a6ba-157d44532fd0',
+        'SignatureVersion' => '1.0',
+        'TemplateCode' => 'SMS_1650053',
+        'Timestamp' => '2016-10-20T05:37:52Z',
+        'Version' => '2016-09-27',
+      }
+      key = 'testsecret'
+      spect_output = 'ka8PDlV7S9sYqxEMRnmlBv%2FDoAE%3D'
+      assert_equal spect_output, Aliyun::Sms::PhoneCode.sign(key, params_input)
+	end
+
+	def test_post_body_data
+		params_input = {
+        'AccessKeyId' => 'testid',
+        'Action' => 'SingleSendSms',
+        'Format' => 'XML',
+        'ParamString' => '{"name":"d","name1":"d"}',
+        'RecNum' => '13098765432',
+        'RegionId' => 'cn-hangzhou',
+        'SignName' => '标签测试',
+        'SignatureMethod' => 'HMAC-SHA1',
+        'SignatureNonce' => '9e030f6b-03a2-40f0-a6ba-157d44532fd0',
+        'SignatureVersion' => '1.0',
+        'TemplateCode' => 'SMS_1650053',
+        'Timestamp' => '2016-10-20T05:37:52Z',
+        'Version' => '2016-09-27',
+      }
+    key = 'testsecret'
+
+    spect_output = 'Signature=ka8PDlV7S9sYqxEMRnmlBv%2FDoAE%3D&AccessKeyId=testid&Action=SingleSendSms&Format=XML&ParamString={"name":"d","name1":"d"}&RecNum=13098765432&RegionId=cn-hangzhou&SignName=标签测试&SignatureMethod=HMAC-SHA1&SignatureNonce=9e030f6b-03a2-40f0-a6ba-157d44532fd0&SignatureVersion=1.0&TemplateCode=SMS_1650053&Timestamp=2016-10-20T05:37:52Z&Version=2016-09-27'
+
+    assert_equal spect_output, Aliyun::Sms::PhoneCode.sign_result(key, params_input)
+	end
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'aliyun/sms/ruby/sdk'
+require 'aliyun/sms'
 
 require 'minitest/autorun'
